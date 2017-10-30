6cccd0e81c83c50a54ac45e294391f55b848ced2
khatamoto-cq <k.hatamoto@caraquri.com>
2017-10-30 10:41:38 +0900

    Webhookのルーティングを実装

---
+++ app/controllers/api/v1/line/base_controller.rb
@@ -0,0 +1,16 @@
+module Api
+  module V1
+    module Line
+      class BaseController < ApplicationController
+        protect_from_forgery with: :null_session
+
+        def client
+          @client ||= Line::Bot::Client.new do |config|
+            config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+            config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+          end
+        end
+      end
+    end
+  end
+end
---
+++ app/controllers/api/v1/line/webhooks_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::Line::WebhooksController < Api::V1::Line::BaseController
+  def callback
+    body = request.body.read
+    logger.info("[Log] body: #{body.inspect}")
+  end
+end
